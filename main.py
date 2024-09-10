b9e8ce7f7d5c5269b406d6f5faf6052d9f74f167
Arthur A. <aires.arthur.15@gmail.com>
2024-09-10 12:41:45 -0300

    add: threads + semaphore

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from log import log_message
 from sf import SFExploit
 from datetime import date
+import threading
 import sys
+
+max_threads = 10
+threads = []
+semaphore = threading.Semaphore(max_threads)
+
+def get_object_items_with_thread_control(object_name, got_objects, vulnerability, tester):
+    with semaphore:
+        object_data = tester.get_object_items(object_name)
+        if object_data:  # Se algo foi retornado
+            log_message(f">>> Found {object_name} to be accessible.")
+            object_data_metric = {object_name: {'total_count': object_data['totalCount']}}
+            vulnerability['accessible_objects'].append(object_data_metric)
+            got_objects.append(object_name)
+
 
 def salesforce_tester(url):
 	log_message(f"> Testing: {url}")
@@ -9,19 +24,26 @@ def salesforce_tester(url):
 					'writable_objects':[]}
 	got_objects = list()
 	tester = SFExploit(url)
+
 	if tester.invalid:
 		return {'vulnerable':False}
+	
 	available_objects = tester.get_objects()
 
 	# test unauth object access
 	log_message(f">> Testing unauth objects.")
+
 	for object_name in available_objects:
-		object_data = tester.get_object_items(object_name)
-		if object_data: # something was returned:
-			log_message(f">>> Found {object_name} to be accessible.")
-			object_data_metric = {object_name:{'total_count':object_data['totalCount']}}
-			vulnerability['accessible_objects'].append(object_data_metric)
-			got_objects.append(object_name)
+		thread = threading.Thread(
+			target=get_object_items_with_thread_control, 
+			args=(object_name, got_objects, vulnerability, tester)
+		)
+		threads.append(thread)
+		thread.start()
+
+	for thread in threads:
+		thread.join()
+
 
 	# test unauth write
 	log_message(f">> Testing unauth write to objects")
